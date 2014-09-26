50709f778b1d506bfddbd8c80131721ce08d12ca
Thomas Calmant <thomas.calmant@isandlatech.com>
2014-09-26 12:02:52 +0200

    Override the multicast port and application ID in demo august 2013

---
+++ demo-august-2013/platforms/base/conf/boot-common.js
@@ -0,0 +1,9 @@
+/*
+ * Sets the ID of the demonstration application
+ */
+{
+    "import-files" : [ "boot-common.js" ],
+    "properties" : {
+        "herald.application.id" : "demo-august-2013"
+    }
+}
---
+++ demo-august-2013/platforms/base/conf/herald/python-transport.js
@@ -0,0 +1,10 @@
+{
+    "import-files" : [ "python-http.js" ],
+
+    "composition" : [ {
+		"name" : "herald-http-discovery-multicast",
+		"properties": {
+		    "multicast.port": 42001
+		}
+	} ]
+}
