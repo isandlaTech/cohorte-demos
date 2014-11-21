b3fb75ba87e708c1ea870c1ead26db679d1196a5
debbabi <dbassem@gmail.com>
2014-11-21 18:09:33 +0100

    adding HTTP startup configs

---
+++ temper/gateway-node/run.js
@@ -0,0 +1,11 @@
+{
+    "node": {                
+        "name": "gateway-node",           
+        "composition-file": "composition.js",
+        "web-admin": 9000,     
+        "shell-admin": 9001
+    },
+    "transport": [
+        "http"
+    ]
+}
---
+++ temper/raspberry-node/run.js
@@ -0,0 +1,11 @@
+{
+    "application-id": "temper",
+    "transport": [
+        "http"
+    ],
+    "node": {
+        "shell-admin": 0,
+        "name": "raspberry-node",
+        "top-composer": false
+    }
+}
